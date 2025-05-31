951dac6505371199c69622222f104c3d1da16a5a
iampawankumar-07 <iampawankumar1997@gmail.com>
2025-05-31 09:47:56 +0000

    Support newline \n as an additional delimiter- Green Phase

--- string_concatenate_kata_node_js/add.js
+++ string_concatenate_kata_node_js/add.js
@@ -2,7 +2,7 @@ function add(numbers) {
   if (numbers === "") return 0;
 
   return numbers
-    .split(',')
+    .split(/[\n,]/) // regex: split on comma or newline
     .map(Number)
     .reduce((sum, n) => sum + n, 0);
 }

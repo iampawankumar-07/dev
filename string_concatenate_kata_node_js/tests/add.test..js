8b76a5eab14a2bb242be539debd33e06210f63e6
iampawankumar-07 <iampawankumar1997@gmail.com>
2025-05-31 09:44:45 +0000

    Handle single number inputs- Red Phase

--- string_concatenate_kata_node_js/tests/add.test..js
+++ string_concatenate_kata_node_js/tests/add.test..js
@@ -3,3 +3,7 @@ const { add } = require('./add');
 test('returns 0 for empty string', () => {
   expect(add("")).toBe(0);
 });
+
+test('returns number itself when one number is provided', () => {
+  expect(add("5")).toBe(5);
+});
